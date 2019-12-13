2e818b49c20907414ca68129b58752da8e0b44c4
wynadorn <janosdevries@gmail.com>
2019-12-13 20:09:04 +0100

    day 9 gold

---
+++ day9/day9-2.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+#include "aocUtil.h"
+#include "computer.h"
+using namespace std;
+
+int main()
+{
+    string input = aocUtil::getChallangeInput(9);
+    //input = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99";    
+    //input = "1102,34915192,34915192,7,4,7,99,0";
+    //input = "104,1125899906842624,99";
+    vector<long long> amplifierCode = aocUtil::explodeLonglong(input, ',');
+  
+    computer c1 = computer(amplifierCode);
+    c1.addInput("2");
+    string output = c1.run();
+
+    cout << output;
+    return 0;
+}
